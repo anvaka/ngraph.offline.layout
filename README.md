2ff056cdbc2e358d0084f204be195dfb05540939
anvaka <anvaka@gmail.com>
2015-05-10 11:17:11 -0700

    Initial implementation

--- README.md
+++ README.md
@@ -1,6 +1,51 @@
 # ngraph.offline.layout
 
-Performs offline layout of large graphs and saves results to the disk
+Performs offline 3D layout of large graphs and saves results to the disk. This is
+very experimental.
+
+The results are saved into `data` folder by default. Note, subsequent runs will
+overwrite content of the folder. Be careful.
+
+# usage
+
+``` js
+// Assume you have a huge graph (instance of ngraph.graph):
+var graph = require('ngraph.generators').grid(10000, 10000);
+var createLayout = require('ngraph.offline.layout');
+var layout = createLayout(graph);
+layout.run();
+```
+
+This will run the 3d `force-based layout` for `500` iterations. Each `5th`
+iteration is saved into `./data/{ITERATION_NUMBER}.bin` file.
+
+Every word in the paragraph above highlighted as `code symbol` can be changed via
+optionsa argument:
+
+``` js
+// run only 100 iterations
+var layout = createLayout(graph, {
+  iterations: 100, // Run `100` iterations only
+  saveEach: 10, // Save each `10th` iteration
+  outDir: './myFolder', // Save results into `./myFolder`
+  layout: require('ngraph.forcelayout3d') // use custom layouter
+});
+```
+
+After all iterations are completed, the final `positions.bin` file will be
+saved into `outDir`. This file consists of Int32's written in Little Endian format.
+Each node of the graph is given three integers in the output file. The order
+of positions matches the order of graph traversal for `graph.forEachNode()`
+method.
+
+If the `outDir` contains data from previous run the layouter will attempt to resume
+based on the last saved iteration. If you don't want this, you can tell it to
+overwrite existing files:
+
+```
+var overwrite = true;
+layout.run(overwrite);
+```
 
 # install
 
---
+++ index.js
@@ -0,0 +1,99 @@
+module.exports = createLayout;
+var path = require('path');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var defaultLayout = require('ngraph.forcelayout3d');
+
+function createLayout(graph, options) {
+  options = options || {};
+  var iterations = typeof options.iterations === 'number' ? options.iterations : 500;
+  var saveEach = typeof options.saveEach === 'number' ? options.saveEach : 5;
+  var outDir = typeof options.outDir === 'string' ? options.outDir : './data';
+  var layouter = typeof options.layout === 'function' ? options.layout : defaultLayout;
+  var layout = layouter(graph);
+  if (!fs.existsSync(outDir)) {
+    mkdirp.sync(outDir);
+  }
+  var lastIteration = getLastIteration(outDir);
+
+  return {
+    run: run,
+    lastIteration: getLastIteration
+  };
+
+  function getLastIteration() {
+    var files = fs.readdirSync(outDir);
+    var largest = 0;
+    for (var i = 0; i < files.length; ++i) {
+      var match = files[i].match(/^(\d+)\.bin$/i);
+      if (!match) continue;
+      var iterationNumber = parseInt(match[1], 10);
+      if (iterationNumber > largest) largest = iterationNumber;
+    }
+    return largest;
+  }
+
+  function run(overwrite) {
+    if (overwrite) {
+      lastIteration = 0;
+    } else {
+      if (lastIteration >= iterations) {
+        printLastIterationHelp();
+        return;
+      } else if (lastIteration > 0) {
+        initLayout(lastIteration);
+      }
+    }
+
+    for (var step = lastIteration + 1; step < iterations; ++step) {
+      console.log('Step ' + step);
+      layout.step();
+      if (step % saveEach === 0) {
+        saveIteration(step);
+      }
+    }
+    saveIteration('positions');
+  }
+
+  function initLayout(iteration) {
+    var lastName = path.join(outDir, iteration + '.bin');
+    console.log('Attempting to resume layout from ' + lastName);
+    var buf = fs.readFileSync(lastName);
+    var idx = 0;
+    graph.forEachNode(initPosition);
+
+    function initPosition(node) {
+      var x = buf.readInt32LE(idx);
+      var y = buf.readInt32LE(idx + 4);
+      var z = buf.readInt32LE(idx + 8);
+      layout.setNodePosition(node.id, x, y, z);
+      idx += 12;
+    }
+  }
+
+  function printLastIterationHelp() {
+    console.log('The ' + outDir + ' already has ' + lastIteration + ' saved iterations.');
+    console.log('* If you want to overwite existing work call `layout.run(true)`');
+    console.log('* If you want to perform more iterations set higher value for `options.iterations`');
+  }
+
+  function saveIteration(name) {
+    var fname = path.join(outDir, name + '.bin');
+
+    console.log("Saving: ", fname);
+    var nodesLength = graph.getNodesCount();
+    var buf = new Buffer(nodesLength * 4 * 3);
+    var i = 0;
+
+    graph.forEachNode(function(node) {
+      var idx = i * 4 * 3;
+      var pos = layout.getNodePosition(node.id);
+      buf.writeInt32LE(pos.x, idx);
+      buf.writeInt32LE(pos.y, idx + 4);
+      buf.writeInt32LE(pos.z, idx + 8);
+      i++;
+    });
+
+    fs.writeFileSync(fname, buf);
+  }
+}
